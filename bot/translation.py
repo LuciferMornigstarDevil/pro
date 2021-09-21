c944608d0d35d400cbe4b4780c4d211abfd01da5
LuciferMornigstarDevil <91133525+LuciferMornigstarDevil@users.noreply.github.com>
2021-09-21 22:04:54 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -29,7 +29,8 @@ class Translation(object):
 
 <b>➥ My Language</b> : <code>Python3</code>
 
-<b>➥ My Library</b> : <i><a href=" </a></i>
+
+<b>➥ My Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
 <b>➥ Source Code</b> : <i><a href="https://github.com/LuciferMornigstarDevil">Click Me</a></i>
 """
